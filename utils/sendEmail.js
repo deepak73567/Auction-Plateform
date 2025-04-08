85f53c43af15d8e6766a70b8dad835cf189a8456
Deepak Vishwakarma <114127594+deepak73567@users.noreply.github.com>
2025-04-08 15:51:51 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,11 +5,13 @@ export const sendEmail = async ({ email, subject, message }) => {
     const transporter = nodeMailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      service: process.env.SMTP_SERVICE,
+      secure: true,
       auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASSWORD,
       },
+      logger: true,     
+      debug: true       
     });
 
     const mailOptions = {
