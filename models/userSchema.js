5b600e0af76813da8bbf36583eb31150234387b3
Deepak Vishwakarma <114127594+deepak73567@users.noreply.github.com>
2025-04-11 23:20:44 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,90 +2,110 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
- const userSchema=new mongoose.Schema({
-    userName:{
-        type:String,
-        minLength:[3,"UserName must contain at least 3 characters."],
-        maxLength:[40,"UserName cant't excced 40 characters."],
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: [true, "UserName is required"],
+    minLength: [3, "UserName must contain at least 3 characters."],
+    maxLength: [40, "UserName can't exceed 40 characters."],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: [8, "Password must contain at least 8 characters."],
+    select: false,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      "Please enter a valid email address",
+    ],
+  },
+  address: String,
+  phone: {
+    type: String,
+    minLength: [10, "Phone Number must contain at least 10 digits."],
+    maxLength: [10, "Phone Number can't exceed 10 digits."],
+  },
+  profileImage: {
+    public_id: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        selected:false,
-        minLength:[8,"Password must contain at least 8 characters."],
-        
+    url: {
+      type: String,
+      required: true,
+    },
+  },
 
+  PaymentMethods: {
+    bankTransfer: {
+      bankAccountNumber: String,
+      bankAccountName: String,
+      bankName: String,
     },
-    email:String,
-    address:String,
-    phone:{
-        type:String,
-        minLength:[10,"Phone Number must contain at least 10 digit."],
-        maxLength:[10,"Phone Number can't excced 10 digit."],
+    GooglePay: {
+      GooglePayAccountNumber: String,
+    },
+    paypal: {
+      paypalEmail: String,
+    },
+  },
 
-    },
-    profileImage:{
-        public_id:{
-            type:String,
-            required:true
-        },
-        url:{
-            type:String,
-            required:true,
-        },
-    },
+  role: {
+    type: String,
+    enum: ["Auctioneer", "Bidder", "Super Admin"],
+    required: true,
+  },
 
-    PaymentMethods:{
-        bankTransfer:{
-            bankAccountNumber:String,
-            bankAccountName:String,
-            bankName:String,
-        },
-        GooglePay:{
-          GooglePayAccountNumber:Number  
-        },
-        paypal:{
-            paypalEmail:String,
-        },
-    },
-     role:{
-     type:String,
-     enum:["Auctioneer","Bidder","Super Admin"]
-     },
-     unpaidCommission:{
-        type:Number,
-        default:0,
-     },
-     auctionWon:{
-        type:Number,
-        default:0
-     },
-     moneySpent:{
-        type:Number,
-        default:0
-     },
-     createdAt:{
-        type:Date,
-        default:Date.now
-     }
+  unpaidCommission: {
+    type: Number,
+    default: 0,
+  },
+  auctionWon: {
+    type: Number,
+    default: 0,
+  },
+  moneySpent: {
+    type: Number,
+    default: 0,
+  },
+
+  resetOTP: {
+    type: String,
+    default: "",
+  },
+  resetOTPExpiry: {
+    type: Date,
+    default: null,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
-userSchema.pre("save",async function (next) {
-    if(!this.isModified("password")){
-        next();
-    }
-    this.password=await bcrypt.hash(this.password,10);
-    
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-userSchema.methods.comparePassword=async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword,this.password);
-}
-userSchema.methods.generateJsonWebToken=function(){
-    console.log(`jwt:${this.id}`);
-    return jwt.sign({id:this._id},process.env.Jwt_SECRET_KEY,{
-        
+// Compare entered password with hashed password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-        expiresIn:process.env.Jwt_EXPIRE,
-    }
-    );
-}
-export const User=mongoose.model("User",userSchema);
+// Generate JWT token
+userSchema.methods.generateJsonWebToken = function () {
+  return jwt.sign({ id: this._id }, process.env.Jwt_SECRET_KEY, {
+    expiresIn: process.env.Jwt_EXPIRE,
+  });
+};
+
+export const User = mongoose.model("User", userSchema);
